d52e4a50b3e92c8631aa1e048916b2d154ab1b67
maximilianoDobal <maximilianodobal@gmail.com>
2021-12-26 16:08:29 -0300

    primer commit

---
+++ src/MiPrimerModulo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MiPrimerModulo = () => {
+    return (
+        <div>
+            <p>Vamos Newellssss</p>
+            <img src="https://c.tenor.com/Jnm3wTb1FRcAAAAC/vamos-newells-weird.gif"></img>
+        </div>
+    )
+}
